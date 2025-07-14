4e4c0be6e710a3e72bad36b342c7647e2e8aab21
shubhankarmarathe-11 <shubhankarsourabhmarathe@gmail.com>
2025-07-14 21:17:45 +0530

    Add encryption & Decryption For data

---
+++ src/Middlewares/EncryptOrDecryptData.js
@@ -0,0 +1,41 @@
+import crypto from "crypto";
+import fs from "fs";
+
+const publicKey = fs.readFileSync("public.pem", "utf8");
+const privateKey = fs.readFileSync("private.pem", "utf8");
+
+const EncryptData = async (data) => {
+  try {
+    const encryptedData = crypto.publicEncrypt(
+      {
+        key: publicKey,
+        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+        oaepHash: "sha256",
+      },
+      Buffer.from(data)
+    );
+
+    return encryptedData.toString("base64");
+  } catch (error) {
+    return false;
+  }
+};
+
+const DecryptData = async (data) => {
+  try {
+    const decryptedData = crypto.privateDecrypt(
+      {
+        key: privateKey,
+        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+        oaepHash: "sha256",
+      },
+      data
+    );
+
+    return decryptedData.toString();
+  } catch (error) {
+    return false;
+  }
+};
+
+export { EncryptData, DecryptData };
