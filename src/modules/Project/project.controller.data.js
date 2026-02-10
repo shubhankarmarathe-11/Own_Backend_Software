0851d8175634b56cc5a3e6e562b607b92dc2f2e0
= <=>
2026-02-10 14:03:58 +0530

    bug fix 02

--- src/modules/Project/project.controller.data.js
+++ src/modules/Project/project.controller.data.js
@@ -4,14 +4,24 @@ import {
   UpdateData,
   DeleteData,
 } from "../Project_Data/project.userdata.services.js";
+import { EncryptData } from "../../utils/encrypt.js";
+import { DecryptData } from "../../utils/Decrypt.js";
 
 const InsertDatacontroller = async (req, res) => {
   try {
     let { project_id, user_id, data } = req.body;
+    console.log(data);
+
+    console.log(JSON.stringify(data));
+
+    let encrypt = await EncryptData(JSON.stringify(data));
+    console.log(encrypt);
+
+    if (encrypt == false) return res.status(406).send("please try again");
     let insert = await InsertData({
       project_id: project_id,
       user_id: user_id,
-      data: data,
+      data: encrypt,
     });
     if (insert == null) return res.status(406).send("please try again");
 
@@ -30,8 +40,16 @@ const RetriveDatacontroller = async (req, res) => {
       user_id: user_id,
     });
     if (retrive == null) return res.status(406).send("please try again");
+    let arr = [];
+    arr.length = 0;
+    for (let val of retrive) {
+      let decrypt = await DecryptData(val.data);
+      if (decrypt == false) return res.status(406).send("please try again");
+      let newdata = JSON.parse(decrypt);
+      arr.push({ _id: val._id, Data_id: val.Data_Id, data: newdata });
+    }
 
-    return res.status(201).send({ data: retrive });
+    return res.status(201).send({ data: arr });
   } catch (error) {
     console.log(error);
     return res.status(400).send("please try again");
@@ -59,11 +77,13 @@ const DeleteDatacontroller = async (req, res) => {
 const UpdateDatacontroller = async (req, res) => {
   try {
     let { project_id, user_id, data_id, newdata } = req.body;
+    let encrypt = await EncryptData(JSON.stringify(newdata));
+    if (encrypt == false) return res.status(406).send("please try again");
     let update = await UpdateData({
       project_id: project_id,
       user_id: user_id,
       data_id: data_id,
-      updateddata: newdata,
+      updateddata: encrypt,
     });
 
     if (update == null) return res.status(406).send("please try again");
